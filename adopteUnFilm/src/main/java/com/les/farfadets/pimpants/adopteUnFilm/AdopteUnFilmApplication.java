f1a6b76ce4bb5e054aed330cb2e9f91b7920b7ba
CecileTalarmin <44167339+CecileTalarmin@users.noreply.github.com>
2019-01-10 15:27:05 +0100

    Add files via upload

---
+++ adopteUnFilm/src/main/java/com/les/farfadets/pimpants/adopteUnFilm/AdopteUnFilmApplication.java
@@ -0,0 +1,14 @@
+package com.les.farfadets.pimpants.adopteUnFilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopteUnFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopteUnFilmApplication.class, args);
+	}
+
+}
+
