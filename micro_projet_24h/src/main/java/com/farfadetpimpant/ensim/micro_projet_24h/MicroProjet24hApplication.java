dc2a60331d7401bfaf5fec68eea5e3afd5fca25f
Unknown <pierreleberre97@gmail.com>
2019-01-10 17:54:03 +0100

    Initial commit le retour

---
+++ micro_projet_24h/src/main/java/com/farfadetpimpant/ensim/micro_projet_24h/MicroProjet24hApplication.java
@@ -0,0 +1,14 @@
+package com.farfadetpimpant.ensim.micro_projet_24h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroProjet24hApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroProjet24hApplication.class, args);
+	}
+
+}
+
---
+++ micro_projet_24h/src/test/java/com/farfadetpimpant/ensim/micro_projet_24h/MicroProjet24hApplicationTests.java
@@ -0,0 +1,17 @@
+package com.farfadetpimpant.ensim.micro_projet_24h;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroProjet24hApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
